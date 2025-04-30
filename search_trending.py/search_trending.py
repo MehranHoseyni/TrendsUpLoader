5dc3061c40fb051b2c3cc8ce3720ec40ed0691e0
MehranHoseyni <hoseyni.m77@gmail.com>
2025-04-30 21:27:23 +0330

    Create search_trending.py

---
+++ search_trending.py/search_trending.py
@@ -0,0 +1,14 @@
+# search_trending.py
+
+def get_trending_keywords():
+    """
+    Returns a list of trending search keywords related to Iran.
+    (Replace this static list with a real API call if available.)
+    """
+    return [
+        "ایران",
+        "تهران",
+        "ماشین ایرانی",
+        "سیاست ایران",
+        "گردشگری ایران"
+    ]
