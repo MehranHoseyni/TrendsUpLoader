e5be9a1a2c76146d986060ed96d0484c9caab03b
MehranHoseyni <hoseyni.m77@gmail.com>
2025-05-03 02:17:16 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 # utils.py
 import json
 
-def save_json(path, data):
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+def save_json(filename: str, data: dict):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
