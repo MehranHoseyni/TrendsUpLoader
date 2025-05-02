c2ad2a0f8bf026375589a6850da9a91698c3b5e2
MehranHoseyni <hoseyni.m77@gmail.com>
2025-05-03 02:12:36 +0330

    Update download_video.py

--- download_video.py
+++ download_video.py
@@ -10,12 +10,21 @@ def download_video(query: str) -> str:
     """
     DOWNLOAD_DIR = 'downloads'
     os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+    # دریافت کوکی‌ها از سکرت
+    cookies = os.environ.get('COOKIES_TXT')
+    
+    if not cookies:
+        raise ValueError("Cookies are not set in the environment variables.")
+
     ydl_opts = {
         'format': 'best[height<=480]',
         'outtmpl': f'{DOWNLOAD_DIR}/%(title)s.%(ext)s',
         'default_search': 'ytsearch',
-        'quiet': True
+        'quiet': True,
+        'cookies': cookies,  # استفاده از کوکی‌های ذخیره شده
     }
+    
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(query, download=True)
         return ydl.prepare_filename(info)
